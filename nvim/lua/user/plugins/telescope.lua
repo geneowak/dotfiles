4a2a7a5035d15f329deab55a48124e1b4c03b427
Eugene Owak <teugeneowak@gmail.com>
2023-07-27 02:16:10 +0300

    Add nvim auto formatting on save.

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -43,17 +43,17 @@ require('telescope').setup({
     lsp_references = {
       previewer = false,
     },
-    extensions = {
-        live_grep_args = {
-          auto_quoting = true, -- enable/disable auto-quoting
-          -- define mappings, e.g.
-          mappings = { -- extend mappings
-            i = {
-              ["<leader>G"] = require('telescope-live-grep-args.actions').quote_prompt({ postfix = " --no-ignore " }),
-            },
-          },
-        }
-      }
+    -- extensions = {
+    --     live_grep_args = {
+    --       auto_quoting = true, -- enable/disable auto-quoting
+    --       -- define mappings, e.g.
+    --       mappings = { -- extend mappings
+    --         i = {
+    --           ["<leader>G"] = require('telescope-live-grep-args.actions').quote_prompt({ postfix = " --no-ignore " }),
+    --         },
+    --       },
+    --     }
+    --   }
   },
 })
 
