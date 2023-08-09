f946945a9171e5bc1bf3776e2997211949502363
Eugene Owak <teugeneowak@gmail.com>
2023-08-09 16:03:56 +0300

    Add neovim prettier.

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -279,6 +279,7 @@ use({
     'b0o/schemastore.nvim',
     'jose-elias-alvarez/null-ls.nvim',
     'jayp0521/mason-null-ls.nvim',
+    'MunifTanjim/prettier.nvim',
   },
   config = function()
     require('user/plugins/lspconfig')
--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -2,73 +2,6 @@
 require('mason').setup()
 require('mason-lspconfig').setup({ automatic_installation = true })
 
--- Switch for controlling whether you want autoformatting.
---  Use :KickstartFormatToggle to toggle autoformatting on or off
-local format_is_enabled = true
-vim.api.nvim_create_user_command('KickstartFormatToggle', function()
-  format_is_enabled = not format_is_enabled
-  print('Setting autoformatting to: ' .. tostring(format_is_enabled))
-end, {})
-
--- Create an augroup that is used for managing our formatting autocmds.
---      We need one augroup per client to make sure that multiple clients
---      can attach to the same buffer without interfering with each other.
-local _augroups = {}
-local get_augroup = function(client)
-  if not _augroups[client.id] then
-    local group_name = 'kickstart-lsp-format-' .. client.name
-    local id = vim.api.nvim_create_augroup(group_name, { clear = true })
-    _augroups[client.id] = id
-  end
-
-  return _augroups[client.id]
-end
-
--- Whenever an LSP attaches to a buffer, we will run this function.
---
--- See `:help LspAttach` for more information about this autocmd event.
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('kickstart-lsp-attach-format', { clear = true }),
-  -- This is where we attach the autoformatting for reasonable clients
-  callback = function(args)
-    local client_id = args.data.client_id
-    local client = vim.lsp.get_client_by_id(client_id)
-    local bufnr = args.buf
-
-    -- Only attach to clients that support document formatting
-    if not client.server_capabilities.documentFormattingProvider then
-      return
-    end
-
-    -- Tsserver usually works poorly. Sorry you work with bad languages
-    -- You can remove this line if you know what you're doing :)
-    if client.name == 'tsserver' then
-      return
-    end
-
-    -- Create an autocmd that will run *before* we save the buffer.
-    --  Run the formatting command for the LSP that has just attached.
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      group = get_augroup(client),
-      buffer = bufnr,
-      callback = function()
-        if not format_is_enabled then
-          return
-        end
-
-        vim.lsp.buf.format {
-          async = false,
-          filter = function(c)
-            return c.id == client.id
-          end,
-        }
-      end,
-    })
-  end,
-})
--- Deprecated
--- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- New way of doing things
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- PHP
@@ -109,8 +42,38 @@ require('lspconfig').eslint.setup({
   end,
 })
 
+local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+local event = "BufWritePre" -- or "BufWritePost"
+local async = event == "BufWritePost"
+
 -- null-ls
 require('null-ls').setup({
+
+    on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.keymap.set("n", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+
+      -- format on save
+      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+      vim.api.nvim_create_autocmd(event, {
+        buffer = bufnr,
+        group = group,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr, async = async })
+        end,
+        desc = "[lsp] format on save",
+      })
+    end
+
+    if client.supports_method("textDocument/rangeFormatting") then
+      vim.keymap.set("x", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+    end
+  end,
+
   sources = {
     require('null-ls').builtins.diagnostics.eslint_d.with({
       condition = function(utils)
