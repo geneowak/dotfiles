7026ae53660d52c4f61276b85744103051970c5a
Eugene Owak <teugeneowak@gmail.com>
2023-12-04 23:53:31 +0300

    Update history icon.

--- nvim/lua/user/plugins/dashboard-nvim.lua
+++ nvim/lua/user/plugins/dashboard-nvim.lua
@@ -12,7 +12,7 @@ dashboard.setup({
     center = {
       { icon = '  ', desc = 'New file', action = 'enew' },
       { icon = '  ', shortcut = 'SPC f', desc = 'Find file', action = 'Telescope find_files' },
-      { icon = '  ', shortcut = 'SPC h', desc = 'Recent files', action = 'Telescope oldfiles' },
+      { icon = '  ', shortcut = 'SPC h', desc = 'Recent files', action = 'Telescope oldfiles' },
       { icon = '  ', shortcut = 'SPC g', desc = 'Find Word', action = 'Telescope live_grep' },
     },
     footer = {}
