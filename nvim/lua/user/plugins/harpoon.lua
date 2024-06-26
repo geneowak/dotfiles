7f41921671e86acade2f134a4c32e6e1a12704c6
Eugene Owak <teugeneowak@gmail.com>
2024-06-26 16:11:01 +0300

    Add harpoon mappings.

--- nvim/lua/user/plugins/harpoon.lua
+++ nvim/lua/user/plugins/harpoon.lua
@@ -7,7 +7,7 @@ return {
     opts = {
         global_settings = {
             -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
-            save_on_toggle = false,
+            save_on_toggle = true,
 
             -- saves the harpoon file upon every change. disabling is unrecommended.
             save_on_change = true,
@@ -30,6 +30,9 @@ return {
             tabline_suffix = "   ",
         }
     },
+    config = function (_, opts)
+        require('harpoon'):setup()
+    end,
     keys = function ()
         local harpoon = require("harpoon")
 
@@ -44,10 +47,15 @@ return {
         -- view all project marks
         {'hm', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end},
         -- navigates to file 3 ... vim.diagnostic.open_float()
-        {"<C-h>", function() harpoon:list():select(1) end},
-        {"<C-t>", function() harpoon:list():select(2) end},
-        {"<C-n>", function() harpoon:list():select(3) end},
-        {"<C-s>", function() harpoon:list():select(4) end},
+        {"h1", function() harpoon:list():select(1) end},
+        {"h2", function() harpoon:list():select(2) end},
+        {"h3", function() harpoon:list():select(3) end},
+        {"h4", function() harpoon:list():select(4) end},
+        {"h5", function() harpoon:list():select(5) end},
+        {"h6", function() harpoon:list():select(6) end},
+        {"h7", function() harpoon:list():select(7) end},
+        {"h8", function() harpoon:list():select(8) end},
+        {"h9", function() harpoon:list():select(9) end},
     }
     end
 }
