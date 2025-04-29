14e2222fa9885005199a3602628cd2e6d6e49699
Eugene Owak <teugeneowak@gmail.com>
2025-04-29 16:46:24 +0300

    Remove github zsh plugins and setup copilot ignore files.

--- nvim/lua/user/plugins/copilot.lua
+++ nvim/lua/user/plugins/copilot.lua
@@ -3,11 +3,19 @@ return {
   {
     "github/copilot.vim",
     config = function()
-      -- Set the keybinding for accepting Copilot suggestions
       -- Disable the default <Tab> mapping
       vim.g.copilot_no_tab_map = true
+
       -- Maps <C-j> (Control + J) to accept Copilot suggestions
       vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+      -- Disable Copilot for specific filetypes
+      -- vim.g.copilot_filetypes = {
+      -- php = false,
+      -- java = false,
+      -- typescript = false,
+      -- go = false,
+      -- }
     end,
   },
 }
